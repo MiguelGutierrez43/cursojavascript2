1a5f1ee9991626eada1d9198eac448adc3aa389f
MiguelGutierrez43 <mgutierrez@siancolors.cl>
2021-03-18 12:50:08 -0300

    Clase 6

---
+++ letyvar.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var numero = 20;
+var texto = 'Hola';
+
+if (true) {
+
+    let numero = 40;
+    let texto = 'Hola a todos';
+
+    console.log(numero);
+    console.log(texto);
+    
+}
+
+console.log(numero);
+console.log(texto);
