2f44a4f7b68743b4397e67add2f5fb2df5966464
Miguel <mgutierrez@siancolors.cl>
2021-03-18 23:17:28 -0300

    Video 8 operadores matematicos

---
+++ operadores.js
@@ -0,0 +1,28 @@
+var numero1 = 10;
+var numero2 = 5;
+
+//Operadores básicos
+
+//Suma
+var respuestasuma = numero1 + numero2;
+
+//Resta
+var respuestaresta = numero1 - numero2;
+
+//Multiplicacion
+var respuestamultiplicacion = numero1 * numero2;
+
+//Division
+var respuestadivision = numero1 / numero2;
+
+console.log("El resultado de la suma es : " + respuestasuma);
+console.log("El resultado de la resta es : " + respuestaresta);
+console.log("El resultado de la multiplicación es : " + respuestamultiplicacion);
+console.log("El resultado de la división es : " + respuestadivision);
+
+//Operadores de Incremento
+respuestasuma ++;
+console.log("Incrementamos ++ " + respuestasuma);
+
+respuestaresta --;
+console.log("Restamos -- " + respuestasuma);
