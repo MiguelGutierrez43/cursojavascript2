028b30184aabb970db79157d7917cd66b0c37869
Miguel <mgutierrez@siancolors.cl>
2021-03-20 20:40:36 -0300

    curso if completo

---
+++ if.js
@@ -0,0 +1,20 @@
+var edad = 19;
+
+if (edad > 18) {
+    
+    console.log('Eres mayor de edad');
+
+}else{
+
+    console.log('Eres menor de edad');
+
+}
+
+if (edad > 18 && edad < 30) {
+
+    console.log('Tu edad esta entre 19 y 30 años');
+    
+}else{
+
+    console.log('Eres demasiado pequeño o demasiado Mayor');
+}
