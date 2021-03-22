12084672431918602e274c7f7444ebbf50508a4d
Miguel <mgutierrez@siancolors.cl>
2021-03-21 23:40:32 -0300

    videos de ventanas emergentes

---
+++ ventanas.js
@@ -0,0 +1,15 @@
+'use strict'
+
+//Alertas dentro del navegador
+
+alert('Mensaje de alerta');
+
+//Ventanas de confirmación
+
+var result = confirm('Cuidado que vas a echar perder tu computador');
+alert(result);
+
+//Ventana de ingreso de datos
+
+var resp = prompt('Enserio quieres echar a perder tu computador','Dijiste que si');
+alert('Has dicho que ' + resp);
