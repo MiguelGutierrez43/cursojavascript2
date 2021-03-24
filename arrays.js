eba4a748190bf1d403a8f02906b21296e4fd7ff2
Miguel <mgutierrez@siancolors.cl>
2021-03-23 23:17:59 -0300

    curso recorrer arrays completo

---
+++ arrays.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var paises = ['Chile','Argentina','Peru','Bolivia'];
+var total = paises.length;
+
+for (var i = 0 ; i <= total ; i++) {
+    
+    console.log(paises[i]);
+    
+}
