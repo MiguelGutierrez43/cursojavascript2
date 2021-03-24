b6347bdea60f19377514402fd07a9c9612f675a5
Miguel <mgutierrez@siancolors.cl>
2021-03-23 23:53:16 -0300

    Clase DOM completa

---
+++ dom.js
@@ -0,0 +1,11 @@
+'use script';
+var div1 = document.getElementById(elementid='div1');
+
+div1.innerHTML = 'Texto cambiado desde javascript';
+
+div1.style.fontSize = '25px';
+
+var parrafo1 = document.getElementsByClassName(className='parrafo1');
+
+parrafo1[1].innerHTML = 'texto cambiado';
+
