50e447ffbe259c71cd6e587773530f366109f36a
MiguelGutierrez43 <mgutierrez@siancolors.cl>
2021-03-31 15:34:35 -0300

    Eventos 4 fin del curso

---
+++ jquery/event4.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$(document).ready(function () {
+    
+    $('#mostrar').click(function () {
+
+        $('#texto').slideToggle(1000, 'swing', function () {
+            
+            console.log('TERMINADO');
+        });
+        
+    });
+});
