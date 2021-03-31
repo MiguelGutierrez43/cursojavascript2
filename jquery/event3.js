701d33846dff66d1e62e1f9e219928eb3db31145
MiguelGutierrez43 <mgutierrez@siancolors.cl>
2021-03-31 15:07:42 -0300

    video eventos 3 completo

---
+++ jquery/event3.js
@@ -0,0 +1,12 @@
+'use strict';
+$(document).ready(function () {
+    
+    $('.parrafo').hover(function () {
+        
+        $(this).addClass('borde');
+
+    }, function () {
+        
+        $(this).removeClass('borde');
+    });
+});
